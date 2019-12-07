25fe502c49f7e05181566ead1dd4a04e04ab3858
korvin001 <geller.m@phystech.edu>
2019-12-08 00:02:19 +0300

    autorization

--- react/my-app/src/components/Login/Login.tsx
+++ react/my-app/src/components/Login/Login.tsx
@@ -2,16 +2,58 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Login: React.FC = () => {
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const onSubmit = React.useCallback(
+        async (event) => {
+            const response = await fetch('http://localhost:8000/api/token/', {
+                method: 'post',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            })
+
+            console.log(response)
+
+            const { access, refresh } = await response.json()
+
+            window.localStorage.setItem('access', access)
+            window.localStorage.setItem('refresh', refresh)
+
+        },
+        [username, password]
+    )
+
+    const onChangeUsername = React.useCallback(
+        (event) => {
+            setUsername(event.target.value)
+        },
+        [username]
+    )
+    
+    const onChangePassword = React.useCallback(
+        (event) => {
+            setPassword(event.target.value)
+        },
+        [password]
+    )
+
+
     return (
         <div className="container">
             <div className="enter">
                 <label className="login-label">Логин:<br />
-                    <input placeholder="Телефон или электронная почта" />
+                    <input placeholder="Телефон или электронная почта" name="name" value={username} onChange={onChangeUsername}/>
                 </label><br /><br />
                 <label className="login-label">Пароль:<br />
-                    <input placeholder="Ваш пароль" />
+                    <input placeholder="Ваш пароль" name= "password" value={password} onChange={onChangePassword}/>
                 </label><br />
-                <Link to="/login" className="enter-button">Войти</Link>
+                <Link to="/login" className="enter-button" onClick={onSubmit}>Войти</Link>
             </div>
         </div>
     )
